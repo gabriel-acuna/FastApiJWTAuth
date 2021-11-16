d3ef1b93b9d3ba2e02ea69aa7aba452b57faa277
gabriel-acuna <gabriel.acuna.reg27@gmail.com>
2021-11-16 15:53:20 -0500

    tablas extrnas + servicio  horas por periodo

--- tests/models/tablas_externas_test.py
+++ tests/models/tablas_externas_test.py
@@ -1,12 +1,18 @@
 from sqlalchemy.sql.expression import select
 from app.models.foreign_tables import *
+from app.services.dac.ServicioHorasPorPerido import *
 
 
 def test_consultar_profesores():
     session = loadSession()
     results =  session.execute(
-        select(Persona.id, Persona.cedula, Persona.nombres).join(Persona.profesor)
+        select(Profesor)
     )
     listado = results.all()
     assert len(listado)>0
-    print(listado)
+    print(listado[0][0].__dict__)
+
+def test_listado_horas_periodo():
+    listado = ServicioHorasPorPerido.verificar_datos_sga(7)
+    assert len(listado)>0
+    print(len(listado))
